ac8f5c05dea9375dc03fc57eff2848ea159df339
Rhuana Pires <rhuanapires@gmail.com>
2020-02-19 17:12:51 -0300

    Updates

---
+++ precache-manifest.dfa58a7aceca2429706ff65f7b93fd76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5131bdb60e94b2af86d44bf5d7c7ef6c",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "819d86016817aab35a3e",
+    "url": "/blog/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9a5259c6b01c921bbb9d",
+    "url": "/blog/static/js/2.dcfc4e55.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/blog/static/js/2.dcfc4e55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "819d86016817aab35a3e",
+    "url": "/blog/static/js/main.c313d386.chunk.js"
+  },
+  {
+    "revision": "cbfd563c8600f0c401a0",
+    "url": "/blog/static/js/runtime-main.3e812e36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blog/static/media/logo.5d5d9eef.svg"
+  }
+]);
