c89a4df0f315e4c1c21875d42ff2b686de300a6e
Rhuana Pires <rhuanapires@gmail.com>
2020-02-19 17:26:31 -0300

    first page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/shooting-star.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="logo-main" alt="Rhuana Pires logo" />
+        <p>Under Construction</p>
       </header>
     </div>
   );
